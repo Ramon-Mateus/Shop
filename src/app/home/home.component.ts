e6798444fc272200d33d0b4a464950cb0ad9acdb
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-07-15 19:01:15 -0300

    feat: add, edit and delete endpoint

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,8 @@ import { PaginatorModule } from 'primeng/paginator';
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
-
+  
+  base_url = "http://localhost:3000"
   products: Product[] = [];
 
   totalRecords: number = 0;
@@ -26,11 +27,58 @@ export class HomeComponent {
 
   fetchProducts(page: number, perPage: number) {
     this.productsService
-      .getProducts('http://localhost:3000/clothes', {page, perPage})
-      .subscribe((products: Products) => {
-        this.products = products.items;
-        this.totalRecords = products.total;
+      .getProducts(`${this.base_url}/clothes`, {page, perPage})
+      .subscribe({
+        next: (data: Products) => {
+          this.products = data.items;
+          this.totalRecords = data.total;
+        },
+        error: (error) => {
+          console.log(error);
+        },
       });
+  }
+
+  editProduct(product: Product, id: number) {
+    this.productsService.editProduct(`${this.base_url}/clothes/${id}`, product).subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+          this.fetchProducts(0, this.rows)
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      }
+    );
+  }
+
+  deleteProduct(id: number) {
+    this.productsService.deleteProduct(`${this.base_url}/clothes/${id}`).subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+          this.fetchProducts(0, this.rows)
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      }
+    );
+  }
+
+  addProduct(product: Product) {
+    this.productsService.editProduct(`${this.base_url}/clothes`, product).subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+          this.fetchProducts(0, this.rows)
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      }
+    );
   }
 
   ngOnInit() { // função chamada quando o componente é inicializado pela primeira vez
